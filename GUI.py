be042ac2d8fdba98690582ab3ac8c0fbd3bfbfb1
4Bondz <bondsfamily2@aol.com>
2020-05-22 21:37:37 -0400

    Pieces and GUI

--- GUI.py
+++ GUI.py
@@ -1,24 +1,28 @@
 from tkinter import *
+import os
 import math
 
 root = Tk()
 root.title("Chess")
 
-pieces = []
+pieces = ["\BlackBishop.png", "./BlackKing.png", "./BlackKnight.png", "./BlackPawn.png", "./BlackQueen.png",
+          "./BlackRook.png", "./WhiteBishop.png", "./WhiteKing.png", "./WhiteKnight.png", "./WhitePawn.png",
+          "./WhiteQueen.png", "./WhiteRook.png"]
 
-p = PhotoImage(file='./WhiteBishop.png')
-
-
-
+loc = os.getcwd()
+print(loc)
 board = []
+
 
 def test():
     pass
 
+
 for l in range(8):
     board.append([])
     for n in range(8):
-        board[l].append(Button(root, text=int(l)*8+n, padx=40, pady=40, command=test, image=p))  # figure out WTF a closure is
+        board[l].append(Button(root, text=int(l) * 8 + n, padx=40, pady=40, command=test,
+                               image=loc + pieces[0]))  # figure out WTF a closure is
 
 # Numbers are Vertical, Letters are Horizontal
 # White is on Bottom, Black is on Top
@@ -27,7 +31,6 @@ for l in range(8):
 for l in range(8):
     for n in range(8):
         board[l][n].grid(row=l, column=n)
-
 
 root.mainloop()
 # while True:
